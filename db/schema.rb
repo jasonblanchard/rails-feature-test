101fef39c3c48c4f364002f12dbdfca456d98f4f
Jason Blanchard <jwb82@nc-114-51.dhcp.drexel.edu>
2012-09-25 15:59:09 -0400

    Fixed the link helper in the revision index

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(:version => 20120925020445) do
     t.integer  "user_id"
   end
 
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.text     "bio"
+  end
+
+  add_index "profiles", ["user_id"], :name => "index_profiles_on_user_id"
+
   create_table "revisions", :force => true do |t|
     t.text     "content"
     t.integer  "post_id"
