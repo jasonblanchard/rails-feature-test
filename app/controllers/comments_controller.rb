48b85131920ad5e94b97f6e552a0c1ff2a0d8bd4
Jason Blanchard <jwb82@nc-114-51.dhcp.drexel.edu>
2012-09-27 12:47:40 -0400

    Comment appears all ajax-ey

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,18 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment])
-    redirect_to post_path(@post)
+    @comment = @post.comments.new(params[:comment])
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to @post}
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @post, :alert => 'Unable to add comment' }
+        format.js { render 'fail_create.js.erb'}
+      end
+    end
   end
 
   def new
