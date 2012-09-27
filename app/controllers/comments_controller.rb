11fd8f3846d4e8ce1142b9c6418d2bdad136c3d7
Jason Blanchard <jwb82@nc-114-51.dhcp.drexel.edu>
2012-09-27 11:20:13 -0400

    Ajax form reveal working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,10 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def new
+    @post = Post.find(params[:post_id])
+  end
+
   def destroy
     @post = current_user.posts.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
